5cfc6a6c0a15b3cc18cfff693b8daa775cc84b27
Arthur Rodrigues <a51550187@gmail.com>
2025-08-28 15:49:08 -0300

    subindo os códigos

---
+++ codigo arthur/meu primeiro codigo.py
@@ -0,0 +1,13 @@
+nome= input("qual é o seu nome?")
+print(nome)
+idade= input("qual é a sua idade?")
+print(idade)
+musica=input('qual é musica favorita?')
+print(musica)
+
+
+
+
+      
+
+
---
+++ codigo arthur/meu seugundo codigo.py
@@ -0,0 +1,40 @@
+#print('arthur'*8000)
+#exemplo de variavel variavel do tipo texto
+fruta=('manga')
+print(type(fruta))
+#variavel do tipo int é inteiro, numero inteiro
+idade = int(35)
+#exemplo de numero quebrado,float
+nota = float(9.5)
+#boalena
+# vc_curte = false 
+
+#exercicio 1 
+
+#criar uma variavel que peça o nome do usuario
+#depois criar uma variavel do tipo float, que pede a nota do usuario
+# depois criar uma variavel que peça a idade do usuario 
+#depois tire um print nas suas variaveis
+
+
+nome = input ("qual é seu nome ")
+print(nome)
+nota = float(input('digete sua nota'))
+print(nota)
+idade = int(input("digete a iadade"))
+print(idade)
+
+####Exercicio 2 
+# crie uma variavel que se chama nome_carro, coloque dentro dessa variavel o carro do seus sonhos 
+# crie uma variavel que se chama preço, coloque dentro dessa variavel o preço do carro
+#rie uma variavel que se chama ano coloque dentro dessa variavel o ano do carro
+#
+nome_carro = ("opala")
+print(nome_carro)
+preço = float (100.000.)
+print(valor)
+ano_do_carro = int('digete o ano')
+
+
+
+
