8ed22bab903b47f429090992e61b3ed3e89ef595
Arthur Rodrigues <a51550187@gmail.com>
2025-10-21 16:48:19 -0300

    Add files via upload

---
+++ EX_1_cadastro.py
@@ -0,0 +1,27 @@
+# Escreva um programa que pede ao usuário o nome, idade, e-mail e senha para um cadastro e depois exiba as informações na tela:
+
+# OUTPUT ESPERADO:
+
+# | ------------------------------ |
+# | ---------- CADASTRO ---------- |
+# | ------------------------------ |
+# | Nome: Maria
+# | Idade: 17
+# | Email: maria@email.com
+# | Senha: 123123
+
+# | ------------------------------ |
+# | ----- USUÁRIO CADASTRADO ----- |
+# | Seja bem vindo(a) Maria!
+# | Email: maria@email.com
+# | ------------------------------ |
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
+nome = input("Qual seu nome?")
+idade = input("Qual sua idade?")
+email = input("qual seu email?")
+senha = input("Qual sua senha?")
+
+print(f"seja bem vindo/a {nome}")
+print(f"email:{email}")
---
+++ EX_2_soma.py
@@ -0,0 +1,14 @@
+# Escreva um programa que pede ao usuário dois números e exiba no final a soma deles:
+# OUTPUT ESPERADO:
+
+# Digite um número: 10
+# Digite outro número: 30
+# A soma entre 10 e 30 é: 40
+
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
+numero = input("Digite um número:")
+segundo_numero = input(" Digite outro número")
+soma = numero + segundo_numero
+print(f"A soma entre {numero} e {segundo_numero} é: {soma}" )
